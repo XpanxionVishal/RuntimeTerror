a988671f4b758a857f8aecf11c138979a907b7ac
Rajat Kumar <rkumar@xpanxion.com>
2020-10-13 12:13:08 +0530

    basic angular code

---
+++ RuntimeTerrorWeb/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'RuntimeTerrorWeb';
+  items: MenuItem[];
+
+  ngOnInit() {
+      this.items = [
+          {label: 'Home', icon: 'pi pi-fw pi-home'},
+          {label: 'Calendar', icon: 'pi pi-fw pi-calendar'},
+          {label: 'Edit', icon: 'pi pi-fw pi-pencil'},
+          {label: 'Documentation', icon: 'pi pi-fw pi-file'},
+          {label: 'Settings', icon: 'pi pi-fw pi-cog'}
+      ];
+  }
+}
