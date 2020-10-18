dc36e27cf8b63e3cfecbeaf6bc44d25767e6fd7c
Rajat Kumar <rkumar@xpanxion.com>
2020-10-18 23:51:39 +0530

    title changes

--- RuntimeTerrorWeb/src/app/app.component.ts
+++ RuntimeTerrorWeb/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RuntimeTerrorWeb';
+  title = 'Safe Home';
   isPostAd = false;
   isAccomodation = false;
   isLogin = false;
